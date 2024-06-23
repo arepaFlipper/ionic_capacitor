4df7ce0348e0f2422dba2d85eab4f17f12cbb2ef
Cristian F. Tovar <cristian00tovar@gmail.com>
2024-06-23 17:18:53 -0500

    🐝 Now it's swiping

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,6 +1,9 @@
 import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
+import Intro1Svg from '../assets/intro/1.svg';
+import Intro2Svg from '../assets/intro/2.svg';
+import Intro3Svg from '../assets/intro/3.svg';
 
 interface ContainerProps {
   onFinish: () => void;
@@ -9,8 +12,9 @@ interface ContainerProps {
 const Intro = ({ onFinish }: ContainerProps) => {
   return (
     <Swiper>
-      <SwiperSlide>test</SwiperSlide>
-      <SwiperSlide>asd</SwiperSlide>
+      <SwiperSlide> <img src={Intro1Svg} alt="Intro 1" /> </SwiperSlide>
+      <SwiperSlide> <img src={Intro2Svg} alt="Intro 2" /> </SwiperSlide>
+      <SwiperSlide> <img src={Intro3Svg} alt="Intro 3" /> </SwiperSlide>
     </Swiper>
   )
 }
