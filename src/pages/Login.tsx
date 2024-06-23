c9f008137d9f3746c0c24ea9186a2fba8262ce23
Cristian F. Tovar <cristian00tovar@gmail.com>
2024-06-23 15:57:13 -0500

    🚥 create a basic form for a login component and tweak styles

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,15 +1,34 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonFooter, IonCard, IonCardContent, IonInput, IonButton } from '@ionic/react';
 type Props = {}
 
 const Login = (props: Props) => {
+  const doLogin = (event: any) => {
+    event.preventDefault();
+    console.log('Logging in...');
+  }
   return (
     <IonPage>
       <IonHeader>
-        <IonToolbar>
-          <IonTitle>Login Page</IonTitle>
+        <IonToolbar color={"success"}>
+          <IonTitle>Title</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent className="ion-padding">UI goes here...</IonContent>
+      <IonContent>
+        <IonCard>
+          <IonCardContent>
+            <form onSubmit={doLogin}>
+              <IonInput fill="outline" labelPlacement="floating" label="Email" type="email" placeholder="crizy182@gmail.com"></IonInput>
+              <IonInput className="ion-margin-top" fill="outline" labelPlacement="floating" label="Password" type="password" placeholder="crizy182@gmail.com"></IonInput>
+              <IonButton className="ion-margin-top" type="submit" expand="full" >
+                Login
+              </IonButton>
+              <IonButton routerLink="/register" color={"secondary"} className="ion-margin-top" type="button" expand="full" >
+                Create Account
+              </IonButton>
+            </form>
+          </IonCardContent>
+        </IonCard>
+      </IonContent>
     </IonPage >
   );
 
