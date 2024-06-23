0cc03005c5a55b18779b6b93d66875cb7574d0a1
Cristian F. Tovar <cristian00tovar@gmail.com>
2024-06-23 17:28:07 -0500

    ⏏️ Style the swipe behavior

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,18 +1,32 @@
-import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { IonButton, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonContent, IonHeader, IonPage, IonText, IonTitle, IonToolbar } from '@ionic/react';
+import { Swiper, SwiperSlide, useSwiper } from 'swiper/react';
 import 'swiper/css';
 import Intro1Svg from '../assets/intro/1.svg';
 import Intro2Svg from '../assets/intro/2.svg';
 import Intro3Svg from '../assets/intro/3.svg';
+import "./Intro.css";
 
 interface ContainerProps {
   onFinish: () => void;
 }
 
+const SwiperButtonNext = ({ children }: any) => {
+  const swiper = useSwiper();
+  return (
+    <IonButton onClick={() => swiper.slideNext()}>{children}</IonButton>
+  )
+}
+
 const Intro = ({ onFinish }: ContainerProps) => {
   return (
     <Swiper>
-      <SwiperSlide> <img src={Intro1Svg} alt="Intro 1" /> </SwiperSlide>
+      <SwiperSlide>
+        <img src={Intro1Svg} alt="Intro 1" />
+        <IonText>
+          <h3>Build awesome apps with Ionic UI components!</h3>
+        </IonText>
+        <SwiperButtonNext>Next</SwiperButtonNext>
+      </SwiperSlide>
       <SwiperSlide> <img src={Intro2Svg} alt="Intro 2" /> </SwiperSlide>
       <SwiperSlide> <img src={Intro3Svg} alt="Intro 3" /> </SwiperSlide>
     </Swiper>
