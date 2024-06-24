0161672934b9ec3680d7de37460d3d224125709b
Cristian F. Tovar <cristian00tovar@gmail.com>
2024-06-24 15:06:33 -0500

    😸 use gestures and animations from Ionic components

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,11 +1,40 @@
 import { useRef } from "react";
-import { CreateAnimation, IonButton, IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar, useIonViewDidEnter } from '@ionic/react';
+import { CreateAnimation, createGesture, Gesture, GestureDetail, IonButton, IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar, useIonViewDidEnter } from '@ionic/react';
 
 const Tab2: React.FC = () => {
   const animationRef = useRef<CreateAnimation | null>(null);
+  const elementRef = useRef<HTMLDivElement | null>(null);
+
+  const onMoveHandler = (detail: GestureDetail) => {
+    const x = detail.currentX - detail.startX;
+    const y = detail.currentY - detail.startY;
+
+    elementRef.current!.style.transform = `translate(${x}px, ${y}px)`;
+  };
+
+  const onMoveEnd = (detail: GestureDetail) => {
+    elementRef.current!.style.transition = "500ms ease-out";
+    elementRef.current!.style.transform = "translate(0px, 0px)";
+  };
+
+  const onStartHandler = (detail: GestureDetail) => {
+    elementRef.current!.style.transition = "none";
+
+  }
+
   useIonViewDidEnter(() => {
-    animationRef.current?.animation.play();
-  })
+    // animationRef.current?.animation.play();
+    const gesture: Gesture = createGesture({
+      threshold: 0,
+      el: elementRef.current!,
+      gestureName: 'my-gesture',
+      onMove: (ev) => onMoveHandler(ev),
+      onEnd: (ev) => onMoveEnd(ev),
+      onStart: (ev) => onStartHandler(ev)
+    });
+    gesture.enable();
+  });
+
   return (
     <IonPage>
       <IonHeader>
@@ -16,7 +45,7 @@ const Tab2: React.FC = () => {
           <IonTitle>Tab 2</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent className="ion-padding">
+      <IonContent className="ion-padding" scrollY={false}>
         <CreateAnimation ref={animationRef} duration={2000} iterations={Infinity} delay={1000}
           keyframes={[
             { offset: 0, transform: 'scale(1)', opacity: '1' },
@@ -27,6 +56,7 @@ const Tab2: React.FC = () => {
             Like this App?
           </IonButton>
         </CreateAnimation>
+        <div ref={elementRef} style={{ height: 50, width: 50, backgroundColor: 'red' }}></div>
       </IonContent>
     </IonPage>
   );
